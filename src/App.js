a57b194d9d5cda929d3b03b02864f802550384d8
Ryan Jones <jonesy_222@hotmail.com>
2019-10-07 23:02:56 +0800

    refactor & load spinner

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import Weather from './components/weather';
+import Loader from './components/loader'
 
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [city, setCity] = useState('Perth');
-  //const [country, setCountry] = useState('');
-  const [temp, setTemp] = useState('');
-  const [humid, setHumid] = useState('');
-  const [desc, setDesc] = useState('');
+  const [selectedCity, setSelectedCity] = useState('Perth');
+  const [weather, setWeather] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    fetchData(city);
-  }, [city]);
-
-  const fetchData = async (city) => {
-    const response = await axios(`https://api.openweathermap.org/data/2.5/weather?q=${city}%2CAustradiva&units=metric&appid=6dc9c2a99870e8a1a5ff5c72ecc15d4e`);
-    setCity(response.data.name);
-    //setCountry(response.data.sys.country);
-    setTemp(response.data.main.temp.toPrecision(3));
-    setHumid(response.data.main.humidity);
-    setDesc(response.data.weather[0].description.replace(/^\w/, c => c.toUpperCase()));
-  };
-
+    const fetchData = async city => {
+      setIsLoading(true);
+      const response = await axios(`https://api.openweathermap.org/data/2.5/weather?q=${city}%2CAustradiva&units=metric&appid=6dc9c2a99870e8a1a5ff5c72ecc15d4e`);
+      setWeather({
+        city: response.data.name,
+        description: response.data.weather[0].description.replace(/^\w/, c => c.toUpperCase()),
+        humidity: response.data.main.humidity,
+        temp: response.data.main.temp.toPrecision(3),
+      });
+      setIsLoading(false);
+    };
+    fetchData(selectedCity);
+  }, [selectedCity]);
 
   const cities = ['Perth', 'Sydney', 'Melbourne', 'Brisbane', 'Adelaide']
 
-  const citySelect = city => fetchData(city);
-
   return (
-    <div className={`container mt-4 ${city} text-white font-weight-bold`}>
+    <div className={`container mt-4 text-white font-weight-bold ${selectedCity}`}>
       <h4 className="display-4 text-center pt-2 mb-5">Australian Weather App</h4>
       <div className="mx-auto row">
-        {cities.map((x, index) => <span onClick={() => citySelect(x)} className="text-center col-sm mb-5" key={index}>{x}</span>)}
+        {cities.map(value => <span key={value} onClick={() => setSelectedCity(value)} className="text-center col-sm mb-5" >{value}</span>)}
       </div>
-      <div className="row">
-        <div className="col text-center city">
-          {city}
-        </div>
-        <Weather temp={temp} desc={desc} humid={humid} />
-      </div>
+      {isLoading ? <Loader /> : !!weather ?
+          <div className="row">
+            <div className="col text-center city">
+              {selectedCity}
+            </div>
+            <Weather {...weather} />
+          </div>
+          : null}
     </div>
   );
 }
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Weather = ({temp, desc, humid}) => {
+const Weather = ({temp, description, humidity}) => {
     return (
         <div className="col text-center">
             <div className="mb-1 temp">{temp}°C</div>
-            <div className="mb-1 description">{desc}</div>
-            <div className="mb-1 humidity">{humid}% Humidity</div>
+            <div className="mb-1 description">{description}</div>
+            <div className="mb-1 humidity">{humidity}% Humidity</div>
         </div>
     )
 }
